74e1b17ac0305a83877132d09b5ee21a8c036e46
Nengyi-Jonathan-Jiang <theNerdyMango@gmail.com>
2021-12-09 11:46:09 -0600

    .

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var canvas_container = document.getElementById("canvas-container");
 var canvas = document.createElement("canvas");
 
+var gl = canvas.getContext("webgl", {antialias:false});
+if(!gl) throw Error("ERROR: WEBGL NOT SUPPORTED");
 
 boilerplate.events.init();
 
@@ -22,34 +24,57 @@ void main(){
     gl_FragColor = vec4(0.5 + 0.5 * cos(fragCoord.xyx + vec3(0,2,4)), 1.0);
 }`)
 
+var _uniform_locs = new Map();
+/**
+ * 
+ * @param {string} name 
+ * @param {"float"|"vec2"|"vec3"|"vec4"|"mat2"|"mat3"|"mat4"|"sampler2D"} type 
+ * @param {number|number[]|WebGLTexture} data 
+ * @returns {void}
+ */
+function setUniform(name, type, data){
+    const loc = _uniform_locs.get(name);
+    if(!loc) _uniform_locs.set(name, loc = gl.getUniformLocation(program, name));
+
+    switch(type){
+        case "float": return gl.uniform1f(loc, data);
+        case "vec2":  return gl.uniform2f(loc, ...data);
+        case "vec3":  return gl.uniform2f(loc, ...data);
+        case "vec4":  return gl.uniform2f(loc, ...data);
+        case "mat2":  return gl.uniform2f(loc, ...data);
+        case "mat3":  return gl.uniform2f(loc, ...data);
+        case "mat4":  return gl.uniform2f(loc, ...data);
+
+        case "sampler2D":
+            gl.activeTexture(gl.TEXTURE0);
+            gl.bindTexture(gl.TEXTURE_2D, data);
+            return gl.uniform1i(gl.getUniformLocation(program, name), 0);
+        
+        default: throw new TypeError(`WEBGL ERROR: Cannot set uniform of type ${type}`);
+    }
+}
 
 var width,height;
 {   //Init canvas
-    var gl = canvas.getContext("webgl", {antialias:false});
-    if(!gl) throw Error("ERROR: WEBGL NOT SUPPORTED");
-
     canvas_container.appendChild(canvas);
     new ResizeObserver((f=>(f(),f))(_=>{
         const {clientWidth, clientHeight} = canvas_container;
         gl.viewport(0, 0, canvas.width = width = clientWidth, canvas.height = height = clientHeight);
-
-        this.matrix = m4.identity()
-        m4.identity(this.matrix);
-          m4.translate(this.matrix,this.pos[0],this.pos[1],0,this.matrix);
-          m4.scale(this.matrix,this.scale/this.aspect,this.scale,1,this.matrix);
-        program.use();
-        gl.uniformMatrix4fv(this.getUniformLoc("u_matrix"), false, );
-        m4.multiply(this.projection_matrix,this.camera_matrix,this.matrix);
-        program.uniformMat4f(matrix_uniform_name,this.matrix);
-
+        gl.useProgram(program);
+        gl.uniformMatrix4fv(gl.getUniformLocation(program, "u_matrix"), false, [height / width,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1]);
     })).observe(canvas_container);
 }
 
+let buffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([-8,-2,-8,2,8,-2,-8,2,8,-2,8,2]), gl.STATIC_DRAW);
+
 function animate(){
     gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);
-    //program.uniform("iTime","FLOAT",now - last_start);
-    const aspectRatio = width / height;
-    t !== this.aspect && (this.aspect = t, this.isMatrixDirty = !0)
-    camera.update_aspect_ratio();
-    camera.draw(program,"u_matrix",gl.TRIANGLES,0,6)
+
+    gl.drawArrays(gl.TRIANGLES, 0, 6);
+
+    requestAnimationFrame(animate);
 }
+
+requestAnimationFrame(animate);
